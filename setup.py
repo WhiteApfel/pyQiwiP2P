7207ce5401790aa5208fd0c2ec30e50c7e53625c
WhiteApfel <github@pfel.ru>
2020-07-08 01:55:07 +0700

    Реорганизация под модуль

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name='pyQiwiP2P',
 	version='1.0',
-	packages=['pyqiwip2p'],
+	packages=['pyqiwip2p', ['pyqiwip2p.qiwi_types']],
 	url='https://github.com/WhiteApfel/pyQiwiP2P',
 	license='MIT Licence',
 	author='WhiteApfel pfel.cc',
