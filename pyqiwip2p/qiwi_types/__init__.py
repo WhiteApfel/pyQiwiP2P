0da1a76223d78a9fafbf1c0db22250ea5e9d7a29
WhiteApfel <github@pfel.ru>
2020-07-08 10:44:34 +0700

    Убрал забытый импорт

--- pyqiwip2p/qiwi_types/__init__.py
+++ pyqiwip2p/qiwi_types/__init__.py
@@ -2,4 +2,3 @@ from pyqiwip2p.qiwi_types.Customer import *
 from pyqiwip2p.qiwi_types.QiwiDatetime import *
 from pyqiwip2p.qiwi_types.Errors import *
 from pyqiwip2p.qiwi_types.Responses import *
-from pyqiwip2p.qiwi_types.CustomFields import *
