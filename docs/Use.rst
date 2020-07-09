c406b9dfd0235cb13c0b3e21003207341a60541b
WhiteApfel <github@pfel.ru>
2020-07-09 18:30:17 +0700

    Небольшие улучшения для документации и пример пользования

--- docs/Use.rst
+++ docs/Use.rst
@@ -8,7 +8,8 @@
 
  QIWI_PRIV_KEY = "abCdef...xYz"
 
- p2p = QiwiP2P(QIWI_PRIV_KEY)
+
+ p2p = QiwiP2P(auth_key=QIWI_PRIV_KEY)
 
  # Выставим счет на сумму 228 рублей который будет работать 45 минут
  new_bill = p2p.bill(bill_id=212332030, amount=228, lifetime=45)
@@ -23,3 +24,18 @@
 
  # Клиент отменил заказ? Тогда и счет надо закрыть
  p2p.reject(bill_id=new_bill.bill_id)
+
+
+ # Если планируете выставлять счета с одинаковой суммой,
+ # можно воспользоваться параметром default_amount
+ p2p = QiwiP2P(auth_key=QIWI_PRIV_KEY, default_amount=148)
+
+ # Теперь, если не указывать в методе p2p.bill() значение суммы заказа,
+ # будет применяться указанная базовая сумма
+ new_bill = p2p.bill(bill_id=6627358)
+
+ # А ещё можно не указывать bill_id, тогда значение сгенерируется автоматически.
+ # Его можно будет посмотреть в объекте ответа Bill
+ # В комбинации со стандартным значением суммы будет вот так
+ new_bill = p2p.bill()
+ print(new_bill.bill_id, new_bill.pay_url)
--- pyqiwip2p/QiwiP2P.py
+++ pyqiwip2p/QiwiP2P.py
@@ -1,5 +1,6 @@
 import typing
-
+import time
+import random
 import requests
 
 from pyqiwip2p.qiwi_types import Bill
@@ -16,14 +17,17 @@ class QiwiP2P:
 
 	:param auth_key: приватный ключ авторизации со страницы https://qiwi.com/p2p-admin/transfers/api. Нужен для работы с вашим аккаунтом.
 	:type auth_key: ``str``
+	:param default_amount: значение суммы счета по умолчанию для новых счетов.
+	:type default_amount: ``int`` or ``float``, optional
 	:param currency: валюта для счетов в формате *Alpha-3 ISO 4217*. Пока что API умеет работать только с *RUB*
 	:type currency: ``str``, optional
 	"""
-	def __init__(self, auth_key: str, currency: str = "RUB"):
+	def __init__(self, auth_key: str, default_amount: int = 100, currency: str = "RUB"):
 		self.auth_key = auth_key
 		self.currency = currency
+		self.default_amount = default_amount
 
-	def bill(self, bill_id: typing.Union[str, int], amount: typing.Union[int, float],
+	def bill(self, bill_id: typing.Union[str, int] = None, amount: typing.Union[int, float] = None,
 				expiration: typing.Union[str, int, QiwiDatetime] = None,
 				lifetime: int = 30,
 				customer: typing.Union[QiwiCustomer, dict] = None, comment: str = "via pyQiwiP2P made by WhiteApfel",
@@ -31,10 +35,10 @@ class QiwiP2P:
 		"""
 		Метод для выставления счета.
 
-		:param bill_id: идентификатор заказа/счета в вашей системе
-		:type bill_id: ``str`` or ``int``
-		:param amount: сумма заказа в рублях. Округляется до двух знаков после запятой.
-		:type amount: ``int`` or ``float``
+		:param bill_id: идентификатор заказа/счета в вашей системе. Если параметр не укзаан, генерируется строка, основанная на времени.
+		:type bill_id: ``str`` or ``int``, optional
+		:param amount: сумма заказа в рублях. Округляется до двух знаков после запятой. Если не указано, используется значение по умолчанию
+		:type amount: ``int`` or ``float``, optional
 		:param expiration: когда счет будет закрыт. Принимает: Timestamp, Datetime или строку формата YYYY-MM-DDThh:mm:ss+hh:mm.
 		:type expiration: ``int``, ``datetime`` or ``str``, optional
 		:param lifetime: время жизни счета в минутах. Если параметр ``expiration`` не указан, то будет автоматически сгенерируется дата для закрытия через ``lifetime`` минут.
@@ -49,6 +53,8 @@ class QiwiP2P:
 		:return: Объект счета при успешном выполнении
 		:rtype: Bill
 		"""
+		bill_id = bill_id if bill_id else f"WhiteApfel-PyQiwiP2P-{str(int(time.time()*100))[4:]}-{int(random.random()*1000)}"
+		amount = amount if amount else self.default_amount
 		expiration = QiwiDatetime(expiration).qiwi if expiration else QiwiDatetime(lifetime).qiwi
 		amount = str(round(float(amount), 2)) if len(str(float(amount)).split(".")[1]) > 1 else str(round(float(amount), 2))+"0"
 		qiwi_request_headers = {
