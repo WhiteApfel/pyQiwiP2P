47730fc1f772579bd13e32b8817344d5aca5ad4d
WhiteApfel <github@pfel.ru>
2020-07-18 14:21:39 +0700

    альфа 2

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
 	name='pyQiwiP2P',
-	version='1.0a1',
+	version='1.0a2',
 	packages=['pyqiwip2p', 'pyqiwip2p.types', 'pyqiwip2p.notify'],
 	url='https://github.com/WhiteApfel/pyQiwiP2P',
 	license='Mozilla Public License 2.0',
 	author='WhiteApfel pfel.cc',
 	author_email='white@pfel.ru',
-	description='pyQiwiP2P'
+	description='pyQiwiP2P',
+	install_requires=['cherrypy', 'phonenumbers', 'email_validator']
 )
