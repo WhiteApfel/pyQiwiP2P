9c24949856abbd6f9e5036601993a23cdc7aa426
WhiteApfel <github@pfel.ru>
2020-07-18 14:09:13 +0700

    Обновление версии

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='pyQiwiP2P',
-	version='v1-1.2',
+	version='1.0a1',
 	packages=['pyqiwip2p', 'pyqiwip2p.types', 'pyqiwip2p.notify'],
 	url='https://github.com/WhiteApfel/pyQiwiP2P',
 	license='Mozilla Public License 2.0',
