d9e7d11e99547ab8d2e4b2f9e4d47761b880f278
white <white@pfel.ru>
2021-04-09 16:11:00 +0300

    1.0a12 с исправлением #5. Скоро во всех киосках страны.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(filename):
 
 setup(
 	name='pyQiwiP2P',
-	version='1.0a11',
+	version='1.0a12',
 	packages=['pyqiwip2p', 'pyqiwip2p.p2p_types', 'pyqiwip2p.notify'],
 	url='https://github.com/WhiteApfel/pyQiwiP2P',
 	license='Mozilla Public License 2.0',
@@ -19,11 +19,11 @@ setup(
 	install_requires=['cherrypy', 'phonenumbers', 'email_validator'],
 	project_urls={
 		"Документальное чтиво": "https://pyqiwip2p.readthedocs.io/ru/latest/",
-		"Донатик": "https://rocketbank.ru/whiteapfel",
+		"Донатик": "https://pfel.cc/donate",
 		"Исходнички": "https://github.com/WhiteApfel/pyQiwiP2P",
 		"Тележка для вопросов": "https://t.me/apfel"
 	},
 	long_description=read('README.md'),
 	long_description_content_type="text/markdown",
-	keywords='qiwip2p api p2p bill tools sdk'
+	keywords='qiwip2p api qiwi p2p payments bill tools sdk'
 )
