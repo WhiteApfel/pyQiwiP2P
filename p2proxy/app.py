dfbb5e7bc1ccae75828542d8afd3ec3dc9b98bb3
Eugene Mayer <white@pfel.ru>
2021-10-19 02:48:33 +0300

    🔥 Remove dotenv, use docker environ

--- p2proxy/app.py
+++ p2proxy/app.py
@@ -1,14 +1,7 @@
 import asyncio
-import os
-
-from dotenv import load_dotenv
 from hypercorn import Config
 from hypercorn.asyncio import serve
 from starlette.applications import Starlette
-
-dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
 
 from server import router
 
