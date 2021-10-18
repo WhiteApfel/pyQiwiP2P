55b0b939ff97b6a9cc97f6ce8b14b5838cec2cb9
Eugene Mayer <white@pfel.ru>
2021-10-19 02:24:55 +0300

    🔥 Remove unused commented code

--- p2proxy/server/proxy.py
+++ p2proxy/server/proxy.py
@@ -1,4 +1,3 @@
-# from models import Referer, RequestReferer, P2ProxyError, ResponseReferer, RequestGetByUid, ResponseReferers
 import os
 
 import validators
@@ -6,24 +5,6 @@ from starlette.responses import RedirectResponse, HTMLResponse
 from starlette.routing import Router
 
 router = Router()
-
-
-# Это не работает, Женя просто попутал что-то...
-#
-# @app.get("/ref/{referer_uid}/bill/{bill_uid}")
-# async def ref_redirect(referer_uid: str, bill_uid: str):
-#     if validators.uuid(bill_uid):
-#         if validators.uuid(referer_uid):
-#             referer = Referer.get_or_none(Referer.uid == referer_uid)
-#             if referer:
-#                 return RedirectResponse(
-#                     url=f"https://oplata.qiwi.com/form?invoiceUid={bill_uid}",
-#                     status_code=308,
-#                     headers={
-#                         "Referer": referer.referer
-#                     }
-#                 )
-#     return "ok"
 
 
 @router.route("/{bill_uid}", "GET")
@@ -49,10 +30,6 @@ async def ref_redirect_qiwi(bill_uid: str):
 @router.route("/bill/{bill_uid}", "GET")
 async def ref_redirect_qiwi_bill(bill_uid: str):
     if validators.uuid(bill_uid):
-        # return RedirectResponse(
-        #     url=f"https://oplata.qiwi.com/form?invoiceUid={bill_uid}",
-        #     status_code=308,
-        # )
         return HTMLResponse(
             f"""
                 <script>
